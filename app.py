55f88388db545e63ac496d3a4f68c12538749281
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-10-30 13:44:18 +0000

    Add app.py, which should have been in the last commit

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import celery
+from celery import Celery
+
+import config
+
+app = Celery('commonshasher', broker=config.BROKER_URL)
+
+app.conf.update(
+    CELERY_ROUTES = {
+        'wmc.process': { 'queue': 'wmc.apidata' },
+        'wmc.update_hash': { 'queue': 'wmc.hash' },
+    },
+)
